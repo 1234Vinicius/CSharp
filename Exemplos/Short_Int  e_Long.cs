21b8fa28e8a0043ba5b876b948478221143c02ca
1234Viniciusgit <99887799vinicius@gmail.com>
2019-08-05 23:52:29 -0300

    Adicionado a pasta exemplos

---
+++ Exemplos/Short_Int  e_Long.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace short_Int_e_Long
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DECLARANDO AS VARIÁVEIS
+            short minhaVarDoTipoShort = 10;
+            System.Int16 minhaVarDoTipoShortDois = 20;
+            int minhaVardoTipoInteiro;
+            long minhaVarDoTipoLong;
+
+            // SOMANDO VALORES DE VARIÁVEIS
+
+            // minhaVarDoTipoShort + minhaVarDoTipoShortDois;
+            // EU NÃO POSSO SIMPLESMENTE SOMAR VALORES, OU FAZER QUALQUER OUTRA OPERAÇÃO CONFORME ACIMA,
+            // DEVO SEMPRE ATRIBUIR O RESULTADO DE UMA OPERAÇÃO A UMA OUTRA VARIÁVEL
+            // OU JÁ IMPRIMIR NA TELA O RESULTADO, CONFORME ABAIXO.
+            Console.WriteLine(minhaVarDoTipoShort + minhaVarDoTipoShortDois);
+            Console.ReadKey();
+        }
+    }
+}
